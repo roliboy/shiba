05072f092a2be8e2a560adf8007bc2b8db1228da
roliboy <roliboy@protonmail.com>
2021-06-30 17:42:06 +0300

    make proxy handle binary data and forward content type

--- src/handlers/proxy.sh
+++ src/handlers/proxy.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# TODO: this
 handle_proxy_response() {
     read -r data
-    data=${data%%$'\r'}
-    
-    read -r PROTOCOL CODE STATUS <<< "$data"
+    read -r PROTOCOL CODE STATUS <<< "${data%%$'\r'}"
 
     # TODO: handle these
     log "PROTOCOL $PROTOCOL"
@@ -14,20 +11,23 @@ handle_proxy_response() {
 
     while read -r line; do
         line=${line%%$'\r'}
-        [ -z "$line" ] && break
+        [[ -z $line ]] && break
         
         if [[ "$line" =~ ^[Cc]ontent-[Ll]ength:[[:space:]](.*)$ ]]; then
             contentlength="${BASH_REMATCH[1]}"
             log "Contentlength: $contentlength"
         fi
+        if [[ "$line" =~ ^[Cc]ontent-[Tt]ype:[[:space:]](.*)$ ]]; then
+            contenttype="${BASH_REMATCH[1]}"
+            log "Contenttype: $contenttype"
+        fi
     done
 
-
-    # TODO: forward content-type
+    # TODO: check if the headers are actually present
     RESPONSE_HEADERS+=("Content-Length: $contentlength")
-    # TODO: set content-type to application/json if the result is valid json?
-    RESPONSE_HEADERS+=("Content-Type: text/plain")
+    RESPONSE_HEADERS+=("Content-Type: $contenttype")
     
+    # TODO: forward status code
     send "HTTP/1.0 200 OK"
     for i in "${RESPONSE_HEADERS[@]}"; do
         send "$i"
@@ -35,26 +35,16 @@ handle_proxy_response() {
     send
 
     head -c "$contentlength" /dev/stdin
-
-    # echo "$content" > /tmp/shibafile
-    # TODO: breaks when too many newlines in result
-    # send_file /tmp/shibafile
-
-    # read -d '@' -r -n "$contentlength" line
-    # echo "$line"
-    # head -c "$contentlength" /dev/stdin | base64
 }
 export -f handle_proxy_response
 
 handle_proxy() {
     method="$1"
     url="$2"
-    body="$(cat)"
 
     log "METHOD: $method"
     log "URL: $url"
-    log "BODY: $body"
-
+    
     # echo -ne "GET ${} Host: localhost\nUser-Agent: shiba-proxy\nAccept: */*"
 
     if [[ "$url" =~ ^([^:]+):([0-9]+)(\/?.*) ]]; then
@@ -66,30 +56,8 @@ handle_proxy() {
         log "PORT: $port"
         log "RESOURCE: $resource"
 
-        # TODO: handle server error
+        # TODO: concatenate data `cat <(echo method resource protocol) <(head -c contentlength /dev/stdin)`
         echo -ne "$method $resource HTTP/1.1\n\n" | nc "$server" "$port" | handle_proxy_response
-        # result=${result%%$'\r'}
-
-        # content="$(handle_proxy_response <<< "$result")"
-        # log "CONTENT: $content"
-
-        # content=${content%%$'\n'}
-        # content="$(tr '\n' ' ' <<< "$content")"
-
-        # TODO: forward content-type
-        # RESPONSE_HEADERS+=("Content-Length: ${#content}")
-        # # TODO: set content-type to application/json if the result is valid json?
-        # RESPONSE_HEADERS+=("Content-Type: text/plain")
-        
-        # send "HTTP/1.0 200 OK"
-        # for i in "${RESPONSE_HEADERS[@]}"; do
-        #     send "$i"
-        # done
-        # send
-
-        # echo "$content" > /tmp/shibafile
-        # # TODO: breaks when too many newlines in result
-        # send_file /tmp/shibafile
     fi
 }
 export -f handle_proxy
