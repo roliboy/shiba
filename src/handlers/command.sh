3993a8256587c08f12d79839398451888c63a56c
roliboy <roliboy@protonmail.com>
2021-07-15 17:02:02 +0300

    update command handler

--- src/handlers/command.sh
+++ src/handlers/command.sh
@@ -1,58 +1,51 @@
 #!/usr/bin/env bash
 
-# TODO: 404 if file does not exist
 handle_command() {
-    command="$1"
-
-    CONTENT_LENGTH="${REQUEST_HEADERS[Content-Length]}"
-
-    IFS= read -d '@' -rn "$CONTENT_LENGTH" stdin
-    stdin=${stdin%%$'\r'}
-    log "STDIN: $stdin"
-    # stdin="$(</dev/stdin)"
-    # stdin="$(cat)"
-    shift
-
     # TODO: make this less spaghett
     read -ra parts <<< "$command"
     command="${parts[0]}"
-    arguments=()
+    args=()
 
     if [[ ${#parts[@]} -gt 1 ]]; then
-        arguments=("${parts[*]:1}")
+        args=("${parts[*]:1}")
     fi
 
-    arguments+=("$@")
+    args+=("$arguments")
 
+    SHIBA_REQUEST_METHOD="$REQUEST_METHOD"
+    export SHIBA_REQUEST_METHOD
 
-    log "COMMAND: $command"
-    log "ARGUMENTS: ${arguments[*]}"
-    log "STDIN $stdin"
+#     TODO: save to file?
 
-    # TODO: prepend ./ when name is not fully qualified?
-
-    if [[ ${#arguments[@]} == 0 ]]; then
-        result="$("$command" <<< "$stdin")"
+    if [[ ${#args[@]} -eq 0 ]]; then
+        if [[ -n $CONTENT_LENGTH ]]; then
+            result="$(head -c "$CONTENT_LENGTH" | "$command")"
+        else
+            result="$("$command" <<< "")"
+        fi
     else
-        log "EXEC: $command ${arguments[*]} | $stdin"
-        result="$("$command" "${arguments[@]}" <<< "$stdin")"
+        if [[ -n $CONTENT_LENGTH ]]; then
+            result="$(head -c "$CONTENT_LENGTH" | "$command" "${args[@]}")"
+        else
+            result="$("$command" "${args[@]}" <<< "")"
+        fi
     fi
 
-    log "RESULT: $result"
 
 
-    # TODO: respond with 500 internal server error if command fails
     RESPONSE_HEADERS+=("Content-Length: ${#result}")
     # TODO: set content-type to application/json if the result is valid json?
     RESPONSE_HEADERS+=("Content-Type: text/plain")
 
-    send "HTTP/1.0 200 OK"
-    for i in "${RESPONSE_HEADERS[@]}"; do
-        send "$i"
-    done
-    send
+    exit_code="$?"
+    if [[ $exit_code -eq 0 ]]; then
+        send "HTTP/1.0 200 OK"
+    else
+        send "HTTP/1.0 500 Internal Server Error"
+    fi
 
-    # TODO: breaks when too many newlines in result
+    send_headers
+
     send "$result"
     log_handler_command_response "$command" "${arguments[*]}"
 }
